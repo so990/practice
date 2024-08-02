9f0fa25af1047c82f182872692d2739e8d3e41e7
hexagon <hexagon@DESKTOP-VU8VC38>
2024-08-02 21:11:57 +0900

    근태,휴가 항목 기능구현 성공

--- JSP_practice/src/attvac_items/command/InsertAttend_itemsHandler.java
+++ JSP_practice/src/attvac_items/command/InsertAttend_itemsHandler.java
@@ -20,23 +20,23 @@ import attvac_items.service.Vacation_itemsRequest;
 import mvc.command.CommandHandler;
 
 public class InsertAttend_itemsHandler implements CommandHandler {
-	private static final String FORM_VIEW = "/WEB-INF/view/attend/vacation.jsp";
-	
+	private static final String FORM_VIEW = "/WEB-INF/view/setting/vacation.jsp";
+
 	private InsertAttend_itemsService insertAttService = new InsertAttend_itemsService();
 	private SelectAttend_itemsService selectAttService = new SelectAttend_itemsService();
 	private InsertVacation_itemsService insertVacService = new InsertVacation_itemsService();
 	private SelectVacation_itemsService selectVacService = new SelectVacation_itemsService();
 
-	SimpleDateFormat fdate = new SimpleDateFormat("yyyy-MM-dd"); //같은 형식으로 맞춰줌
-    Date vac_start;
-    Date vac_end;
-	
+	SimpleDateFormat fdate = new SimpleDateFormat("yyyy-MM-dd"); // 같은 형식으로 맞춰줌
+	Date vac_start;
+	Date vac_end;
+
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		if(req.getMethod().equalsIgnoreCase("get")) {		//get 요청이 오면 FORM_VIEW
-			return processForm(req,res);
-		}else if(req.getMethod().equalsIgnoreCase("post")) {//post 요청이 오면 processSubmit()로 이동
-			return processSubmit(req,res);
+		if (req.getMethod().equalsIgnoreCase("get")) { // get 요청이 오면 FORM_VIEW
+			return processForm(req, res);
+		} else if (req.getMethod().equalsIgnoreCase("post")) {// post 요청이 오면 processSubmit()로 이동
+			return processSubmit(req, res);
 		} else {
 			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 			return null;
@@ -44,22 +44,23 @@ public class InsertAttend_itemsHandler implements CommandHandler {
 	}
 
 	private String processForm(HttpServletRequest req, HttpServletResponse res) {
-		
+
 		List<Vacation_items> list_vac = selectVacService.select();
 		req.setAttribute("list_vac", list_vac);
-		
+
 		List<Attend_items> list_att = selectAttService.select();
 		req.setAttribute("list_att", list_att);
-		
+
 		return FORM_VIEW;
 	}
-	
+
 	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
 		
 		Map<String, Boolean> errors = new HashMap<>();
 		req.setAttribute("errors", errors);
 		
 		try { 
+			
 			if (req.getParameter("vac_start") == null || req.getParameter("vac_start").isEmpty()) {
 				vac_start = null;
 			}else {
@@ -71,55 +72,76 @@ public class InsertAttend_itemsHandler implements CommandHandler {
 			}else {
 				vac_end = Date.valueOf(req.getParameter("vac_end"));
 			}
-			Vacation_itemsRequest vacReq = new Vacation_itemsRequest(
-					req.getParameter("vac_name"),
-					vac_start,
-					vac_end,
-					req.getParameter("vac_used")
-					);
 			
-			Attend_itemsRequest attReq = new Attend_itemsRequest(
-					req.getParameter("att_name"),
-					req.getParameter("att_unit"),
-					req.getParameter("att_grp"),
-					req.getParameter("att_deduction"),
-					req.getParameter("att_conn"),
-					req.getParameter("att_used")
-					);
+			////////////////////////////////////////////
+			
+			Vacation_itemsRequest vacReq = null;
+			Attend_itemsRequest attReq = null;
+			
+			if(req.getParameter("vac_name") != null) {
+				vacReq = new Vacation_itemsRequest(
+						req.getParameter("vac_name"),
+						vac_start,
+						vac_end,
+						req.getParameter("vac_used")
+						);
+				
+				vacReq.validate(errors);
+				
+				if(!errors.isEmpty()) {	// 에러가있으면 newArticleForm 주소를 반환
+					
+					List<Vacation_items> list_vac = selectVacService.select();
+					req.setAttribute("list_vac", list_vac);
+					
+					List<Attend_items> list_att = selectAttService.select();
+					req.setAttribute("list_att", list_att);
+					
+					return FORM_VIEW;
+				}
+				
+				Vacation_items vacation_items = insertVacService.insert(vacReq);
+			}
+			
+			if(req.getParameter("att_name") != null) {
+				attReq = new Attend_itemsRequest(
+						req.getParameter("att_name"),
+						req.getParameter("att_unit"),
+						req.getParameter("att_grp"),
+						req.getParameter("att_deduction"),
+						req.getParameter("att_conn"),
+						req.getParameter("att_used")
+						);
+				attReq.validate(errors);		
+				
+				
+				if(!errors.isEmpty()) {	// 에러가있으면 newArticleForm 주소를 반환
+					List<Vacation_items> list_vac = selectVacService.select();
+					req.setAttribute("list_vac", list_vac);
+					
+					List<Attend_items> list_att = selectAttService.select();
+					req.setAttribute("list_att", list_att);
+					
+					return FORM_VIEW;
+				}
 
-			attReq.validate(errors);
-		
-			List<Vacation_items> list_vac = selectVacService.select();
-			req.setAttribute("list_vac", list_vac);
-			
-			List<Attend_items> list_att = selectAttService.select();
-			req.setAttribute("list_att", list_att);
-			
-			if(!errors.isEmpty()) {	// 에러가있으면 newArticleForm 주소를 반환
-				return FORM_VIEW;
-		}
-			
-			Attend_items attend_items = insertAttService.insert(attReq);
-			Vacation_items vacation_items = insertVacService.insert(vacReq);
-			
-			list_vac = selectVacService.select();
-			req.setAttribute("list_vac", list_vac);
-			
-			list_att = selectAttService.select();
-			req.setAttribute("list_att", list_att);
-			
-			
-		
-		//작성과 DB에 저장이 성공적으로 완료되면 저장
-		
-		
+				Attend_items attend_items = insertAttService.insert(attReq);  
+
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		List<Vacation_items> list_vac = selectVacService.select();
+		req.setAttribute("list_vac", list_vac);
+		
+		List<Attend_items> list_att = selectAttService.select();
+		req.setAttribute("list_att", list_att);
+		
 		return FORM_VIEW;	// 수정필요. 새창만들기? 리퀘스트에 올려서 팝업띄우기?
 		//newArticleSuccess 주소를 반환
+		
 	}
-	
+
 }
